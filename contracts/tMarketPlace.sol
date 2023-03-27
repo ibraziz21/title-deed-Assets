ceb4dc36208288c393626b0989289b03e6313d2f
Ibrahim Aziz <ibraziz21@gmail.com>
2023-03-27 15:40:51 +0300

    Update tMarketPlace.sol

--- contracts/tMarketPlace.sol
+++ contracts/tMarketPlace.sol
@@ -3,23 +3,44 @@ pragma solidity 0.8.17;
 
 import "./titleNFT.sol";
 contract MarketPlace {
-    constructor() {
-        
-    }
+    error notYours();
+    error InvalidInput();
 
-    function setNFTForSale()  returns () {
-        
-    }
-
-    function setForRent()  returns () {
-        
-    }
-
-    function buyNFT()  returns () {
-        
-    }
-    function rentNFT()  returns () {
-        
-    }
+    titleNFT public nfts;
     
+    uint public salesCount;
+    struct NFTSale {
+        uint _tokenId;
+        uint _price;
+        bool _sellActive;
+
+    }
+    mapping (uint => NFTSale) public saleDetails;
+    
+    constructor(address _titleNFT) {
+        if(_titleNFT == address(0)) revert InvalidInput();
+        nfts = titleNFT(_titleNFT);
+        salesCount = 0;
+            }
+
+    function setNFTForSale(uint _id, uint sellPrice)external {
+        if(nfts.ownerOf(_id)!=msg.sender) revert notYours();
+        uint saleId = ++salesCount;
+            NFTSale storage sale = saleDetails[saleId];
+            sale._tokenId = _id;
+            sale._price = sellPrice;
+            sale._sellActive;
+        }
+
+    function setForRent() external {
+        
+    }
+
+    function buyNFT() external  {
+        
+    }
+    function rentNFT() external {
+        
+    }
+
 }
