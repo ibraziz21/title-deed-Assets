b2abc182f676a291d887aace3ab0b4a883a69006
Ibrahim Aziz <ibraziz21@gmail.com>
2023-03-27 14:41:30 +0300

    Create tMarketPlace.sol

---
+++ contracts/tMarketPlace.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.17;
+
+import "./titleNFT.sol";
+contract MarketPlace {
+    constructor() {
+        
+    }
+
+    function setNFTForSale()  returns () {
+        
+    }
+
+    function setForRent()  returns () {
+        
+    }
+
+    function buyNFT()  returns () {
+        
+    }
+    function rentNFT()  returns () {
+        
+    }
+    
+}
